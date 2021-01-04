d39daf6431c9821b52222f1c4fa1d19c1c1cb807
Kirill Savinov <kirill.savinov@gmail.com>
2021-01-04 15:44:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Snake 3D, made with Ursina Engine.
+
+Controls:
+- **"W"** - Up
+- **"A"** - Left 
+- **"S"** - Down 
+- **"D"** - Right
+
+
+- **"2"** - 2D view
+- **"3"** - 3D view
+
+
+- **"P"** - PAUSE
+- **"ESC"** - Exit
---
+++ main.py
@@ -0,0 +1,104 @@
+from game_objects import *
+import sqlite3
+
+# ----------------- for saving records to db ---------------------------------
+db = sqlite3.connect("server.db")
+cur = db.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS records (
+    record TEXT
+)""")
+db.commit()
+
+cur.execute("SELECT record FROM records")
+if cur.fetchone() is None:
+    cur.execute(f"INSERT INTO records VALUES (?)", "0")
+    db.commit()
+# ----------------------------------------------------------------------
+
+
+class Game(Ursina):
+    def __init__(self):
+        self.apple = ""
+        self.snake = ""
+        super().__init__()
+        window.color = color.black
+        window.fullscreen_size = 1920, 1080
+        window.fullscreen = True
+        Light(type="ambient", color=(0.5, 0.5, 0.5, 1))
+        Light(type="directional", color=(0.5, 0.5, 0.5, 1), directional=(1, 1, 1))
+        self.MAP_SIZE = 20
+        self.new_game()
+        camera.position = (self.MAP_SIZE // 2, -20.5, -20)
+        camera.rotation_x = -57
+        self.record = "0"
+
+    def create_map(self, MAP_SIZE):
+        Entity(model="quad", scale=MAP_SIZE, position=(MAP_SIZE // 2, MAP_SIZE // 2, 0), color=color.dark_gray)
+        Entity(model=Grid(MAP_SIZE, MAP_SIZE), scale=MAP_SIZE,
+               position=(MAP_SIZE // 2, MAP_SIZE // 2, -0.01), color=color.white)
+
+    def new_game(self):
+        scene.clear()
+        self.create_map(self.MAP_SIZE)
+        self.apple = Apple(self.MAP_SIZE, model="sphere", color=color.red)
+        self.snake = Snake(self.MAP_SIZE)
+
+    def input(self, key):
+        """
+        Handles pressed keys
+        :param key:
+        :return:
+        """
+        if key == 'escape':
+            quit()
+        if key == 'p':
+            if not application.paused:
+                application.pause()
+            else:
+                application.resume()
+        if key == '2':
+            camera.rotation_x = 0
+            camera.position = (self.MAP_SIZE // 2, self.MAP_SIZE // 2, -50)
+        elif key == '3':
+            camera.position = (self.MAP_SIZE // 2, -20.5, -20)
+            camera.rotation_x = -57
+        super().input(key)
+
+    def check_apple_eaten(self):
+        if self.snake.segment_positions[-1] == self.apple.position:
+            self.snake.add_segment()
+            self.apple.new_position()
+
+    def check_game_over(self):
+        snake = self.snake.segment_positions
+        if 0 < snake[-1][0] < self.MAP_SIZE and 0 < snake[-1][1] < self.MAP_SIZE and len(snake) == len(set(snake)):
+            self.set_record(self.record, self.snake.score)
+            return
+        print_on_screen('GAME OVER', position=(-0.7, 0.1), scale=10, duration=1)
+        self.snake.direction = Vec3(0, 0, 0)
+        self.snake.permissions = dict.fromkeys(self.snake.permissions, 0)
+        invoke(self.new_game, delay=1)
+
+    def get_record(self):
+        for value in cur.execute("SELECT record FROM records"):
+            return value[0]
+
+    def set_record(self, record, score):
+        rec = max(int(record), score)
+        cur.execute(f"UPDATE records SET record = {str(rec)}")
+        db.commit()
+
+    def update(self):
+        """ Updates for every frame """
+        self.record = self.get_record()
+        print_on_screen(f'Score:     {self.snake.score}\nRecord:  {self.record}', position=(-0.85, 0.45), scale=3, duration=1 / 20)
+        self.check_apple_eaten()
+        self.check_game_over()
+        self.snake.run()
+
+
+if __name__ == '__main__':
+    game = Game()
+    update = game.update
+    game.run()
